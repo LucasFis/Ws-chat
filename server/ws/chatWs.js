4e8d971173a9048643a7009670e9cac4e2c1cd57
LucasFis <lfiscella@frba.utn.edu.ar>
2026-01-03 16:22:11 -0300

    Mejor distribucion en chatWs

--- server/ws/chatWs.js
+++ server/ws/chatWs.js
@@ -7,32 +7,16 @@ const rooms = new Map();
 export const configureWsChat = (wss) => {
     const chatRepo = new ChatRepositorio();
 
-    wss.on("connection", async (ws, req) => {
+    wss.on("connection", (ws, req) => {
 
         console.log("ws://chat connected")
 
         const url = new URL(req.url, "http://localhost");
         const chatId = url.searchParams.get("chatId");
 
-        if (!chatId) {
-            ws.close(1008, "chatId requerido");
-            return;
-        }
 
-        if (!rooms.has(chatId)) {
-            rooms.set(chatId, new Set());
-        }
 
-        rooms.get(chatId).add(ws);
-
-        const chat = await chatRepo.findById(chatId);
-
-        ws.send(JSON.stringify({
-            type: "INIT_CHAT",
-            data: chatADTO(chat)
-        }))
-
-        console.log("ws://chat sended ", chatId)
+        init(ws, chatId, chatRepo);
 
         ws.on("message", (msg) => {
             const payload = JSON.parse(msg.toString())
@@ -44,6 +28,29 @@ export const configureWsChat = (wss) => {
             console.log("ws://chat disconnected")
         });
     });
+}
+
+const init = async (ws, chatId, chatRepo) => {
+
+    if (!chatId) {
+        ws.close(1008, "chatId requerido");
+        return;
+    }
+
+    if (!rooms.has(chatId)) {
+        rooms.set(chatId, new Set());
+    }
+
+    rooms.get(chatId).add(ws);
+
+    const chat = await chatRepo.findById(chatId);
+
+    ws.send(JSON.stringify({
+        type: "INIT_CHAT",
+        data: chatADTO(chat)
+    }))
+
+    console.log("ws://chat sended ", chatId)
 }
 
 const messageHandler = async (event, chatId, chatRepo) => {
